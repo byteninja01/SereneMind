8042da14b077a970e36599ed2635659f8629d712
Ojus Mathur <ojusmathur17@gmail.com>
2025-10-16 07:47:01 +0000

    ai chatbot is not working pls check

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,8 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'gemini-1.5-flash-latest',
+  plugins: [googleAI({
+      apiVersion: "v1beta"
+  })],
+  model: googleAI('gemini-1.5-flash-latest'),
 });
