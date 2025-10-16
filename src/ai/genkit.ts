36208b8c5e112758780cf4568ba42f968a3cf522
Ojus Mathur <ojusmathur17@gmail.com>
2025-10-16 07:49:30 +0000

    if not proper then just give normal response

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,8 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({
-      apiVersion: "v1beta"
-  })],
-  model: googleAI('gemini-1.5-flash-latest'),
+  plugins: [googleAI()],
+  model: googleAI('gemini-pro'),
 });
